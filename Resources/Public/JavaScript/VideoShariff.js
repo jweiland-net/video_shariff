6f0fd7c21eebff1c46555187a73a43faa1becaea
Jan Kiesewetter <jan@t3easy.de>
2020-02-14 22:04:11 +0100

    [TASK] Directly write video html to data-video
    Resolves: #17

--- Resources/Public/JavaScript/VideoShariff.js
+++ Resources/Public/JavaScript/VideoShariff.js
@@ -3,7 +3,7 @@ function replaceVideo(event) {
     while (previewLink.className !== 'video-shariff-play') {
         previewLink = previewLink.parentElement;
     }
-    previewLink.outerHTML = TYPO3.settings['video_shariff']['video'][previewLink.dataset.video];
+    previewLink.outerHTML = JSON.parse(previewLink.dataset.video);
 }
 
 var videos = document.getElementsByClassName('video-shariff-play'), i = 0;
