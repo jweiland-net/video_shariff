b2c5a8d58c7b90efa69a3e3065eab8e5f173455b
Stefan Froemken <froemken@gmail.com>
2022-09-02 22:51:46 +0200

    Update version to 2.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,13 +4,13 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'This extension provides more privacy when embedding videos in frontend.',
     'category' => 'plugin',
     'state' => 'stable',
-    'author' => 'Pascal Rinker',
+    'author' => 'Stefan Froemken',
     'author_email' => 'projects@jweiland.net',
     'author_company' => 'jweiland.net',
-    'version' => '2.0.0',
+    'version' => '2.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-11.5.99',
+            'typo3' => '10.4.29-11.5.99',
             'fluid_styled_content' => '10.4.0-0.0.0'
         ],
         'conflicts' => [],
