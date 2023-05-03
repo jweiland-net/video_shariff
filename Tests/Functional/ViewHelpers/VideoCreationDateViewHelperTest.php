8360ef94c76a6bf2388d558c8bcc0c83852323a7
Stefan Froemken <sfroemken@jweiland.net>
2023-05-03 17:05:03 +0200

    Add further tests for ViewHelper classes

--- Tests/Functional/ViewHelpers/VideoCreationDateViewHelperTest.php
+++ Tests/Functional/ViewHelpers/VideoCreationDateViewHelperTest.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
  * LICENSE file that was distributed with this source code.
  */
 
-namespace JWeiland\VideoShariff\ViewHelpers;
+namespace JWeiland\VideoShariff\Tests\Functional\ViewHelpers;
 
 use TYPO3\CMS\Core\Resource\File;
 use TYPO3\CMS\Core\Resource\FileReference;
@@ -23,138 +23,166 @@ use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
  */
 class VideoCreationDateViewHelperTest extends FunctionalTestCase
 {
-    protected VideoCreationDateViewHelper $subject;
+    protected array $testExtensionsToLoad = [
+        'typo3conf/ext/video_shariff'
+    ];
 
-    /**
-     * This method is called before each test.
-     */
     protected function setUp(): void
     {
+        if (!defined('ORIGINAL_ROOT')) {
+            define('ORIGINAL_ROOT', $_ENV['TYPO3_PATH_ROOT']);
+        }
+
         parent::setUp();
-
-        $this->subject = new VideoCreationDateViewHelper();
-    }
-
-    public function fileDataProvider(): array
-    {
-        $file1 = $this->createMock(File::class);
-        $file1
-            ->expects(self::atLeastOnce())
-            ->method('getUid')
-            ->willReturn(1);
-        $file1
-            ->expects(self::atLeastOnce())
-            ->method('getProperty')
-            ->with(self::identicalTo('content_creation_date'))
-            ->willReturn(1683100800);
-        $file1
-            ->expects(self::never())
-            ->method('getProperty')
-            ->with(self::identicalTo('creation_date'));
-        $file1
-            ->expects(self::never())
-            ->method('getProperty')
-            ->with(self::identicalTo('crdate'));
-
-        $coreFileReference1 = $this->createMock(FileReference::class);
-        $coreFileReference1
-            ->expects(self::atLeastOnce())
-            ->method('getOriginalFile')
-            ->willReturn($file1);
-
-        $extbaseFileReference1 = new ExtbaseFileReference();
-        $extbaseFileReference1->setOriginalResource($coreFileReference1);
-
-        $file2 = $this->createMock(File::class);
-        $file2
-            ->expects(self::atLeastOnce())
-            ->method('getUid')
-            ->willReturn(1);
-        $file2
-            ->expects(self::atLeastOnce())
-            ->method('getProperty')
-            ->with(self::identicalTo('content_creation_date'))
-            ->willReturn(0);
-        $file2
-            ->expects(self::atLeastOnce())
-            ->method('getProperty')
-            ->with(self::identicalTo('creation_date'))
-            ->willReturn(1682928000);
-        $file2
-            ->expects(self::never())
-            ->method('getProperty')
-            ->with(self::identicalTo('crdate'));
-
-        $coreFileReference2 = $this->createMock(FileReference::class);
-        $coreFileReference2
-            ->expects(self::atLeastOnce())
-            ->method('getOriginalFile')
-            ->willReturn($file2);
-
-        $extbaseFileReference2 = new ExtbaseFileReference();
-        $extbaseFileReference2->setOriginalResource($coreFileReference2);
-
-        $file3 = $this->createMock(File::class);
-        $file3
-            ->expects(self::atLeastOnce())
-            ->method('getUid')
-            ->willReturn(1);
-        $file3
-            ->expects(self::atLeastOnce())
-            ->method('getProperty')
-            ->with(self::identicalTo('content_creation_date'))
-            ->willReturn(0);
-        $file3
-            ->expects(self::atLeastOnce())
-            ->method('getProperty')
-            ->with(self::identicalTo('creation_date'))
-            ->willReturn(0);
-        $file3
-            ->expects(self::atLeastOnce())
-            ->method('getProperty')
-            ->with(self::identicalTo('crdate'))
-            ->willReturn(1683014400);
-
-        $coreFileReference3 = $this->createMock(FileReference::class);
-        $coreFileReference3
-            ->expects(self::atLeastOnce())
-            ->method('getOriginalFile')
-            ->willReturn($file3);
-
-        $extbaseFileReference3 = new ExtbaseFileReference();
-        $extbaseFileReference3->setOriginalResource($coreFileReference3);
-
-        return [
-            'Extbase FileReference with content_creation_date returns 1683100800' => [$file1, 1683100800],
-            'Core FileReference with content_creation_date returns 1683100800' => [$file1, 1683100800],
-            'Extbase FileReference with content_creation_date returns 1682928000' => [$file2, 1682928000],
-            'Core FileReference with content_creation_date returns 1682928000' => [$file2, 1682928000],
-            'Extbase FileReference with content_creation_date returns 1683014400' => [$file3, 1683014400],
-            'Core FileReference with content_creation_date returns 1683014400' => [$file3, 1683014400],
-        ];
     }
 
     /**
      * @test
-     *
-     * @dataProvider fileDataProvider
      */
-    public function videoCreateDateReturnContentCreationDate($file, int $expects): void
+    public function videoCreateDateReturnContentCreationDate(): void
     {
-        $view = GeneralUtility::makeInstance(StandaloneView::class);
-        $view->assign('file', $file);
-        $view->setTemplateSource('
-            <html lang="en"
-                  xmlns:f="http://typo3.org/ns/TYPO3/CMS/Fluid/ViewHelpers"
-                  xmlns:jw="http://typo3.org/ns/JWeiland/VideoShariff/ViewHelpers"
-                  data-namespace-typo3-fluid="true">
-                  {jw:videoCreationDate(fileReference: \'{file}\')}
-            </html>
-        ');
+        $file = $this->createMock(File::class);
+        $file
+            ->expects(self::atLeastOnce())
+            ->method('getUid')
+            ->willReturn(1);
+        $file
+            ->expects(self::atLeastOnce())
+            ->method('getProperty')
+            ->willReturnMap([
+                ['content_creation_date', 1683100800],
+                ['creation_date', 0],
+                ['crdate', 0],
+            ]);
 
+        $coreFileReference = $this->createMock(FileReference::class);
+        $coreFileReference
+            ->expects(self::atLeastOnce())
+            ->method('getOriginalFile')
+            ->willReturn($file);
+
+        $extbaseFileReference = new ExtbaseFileReference();
+        $extbaseFileReference->setOriginalResource($coreFileReference);
+
+        $view = GeneralUtility::makeInstance(StandaloneView::class);
+        $view->setTemplateSource(implode(' ', [
+            '<html lang="en" xmlns:f="http://typo3.org/ns/TYPO3/CMS/Fluid/ViewHelpers"',
+            'xmlns:jw="http://typo3.org/ns/JWeiland/VideoShariff/ViewHelpers"',
+            'data-namespace-typo3-fluid="true">',
+            '{jw:videoCreationDate(fileReference: \'{file}\')}',
+            '</html>'
+        ]));
+
+        $view->assign('file', $coreFileReference);
         self::assertSame(
-            $expects,
-            $view->render()
+            '1683100800',
+            trim($view->render())
+        );
+
+        $view->assign('file', $extbaseFileReference);
+        self::assertSame(
+            '1683100800',
+            trim($view->render())
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function videoCreateDateReturnCreationDate(): void
+    {
+        $file = $this->createMock(File::class);
+        $file
+            ->expects(self::atLeastOnce())
+            ->method('getUid')
+            ->willReturn(1);
+        $file
+            ->expects(self::atLeastOnce())
+            ->method('getProperty')
+            ->willReturnMap([
+                ['content_creation_date', 0],
+                ['creation_date', 1682928000],
+                ['crdate', 0],
+            ]);
+
+        $coreFileReference = $this->createMock(FileReference::class);
+        $coreFileReference
+            ->expects(self::atLeastOnce())
+            ->method('getOriginalFile')
+            ->willReturn($file);
+
+        $extbaseFileReference = new ExtbaseFileReference();
+        $extbaseFileReference->setOriginalResource($coreFileReference);
+
+        $view = GeneralUtility::makeInstance(StandaloneView::class);
+        $view->setTemplateSource(implode(' ', [
+            '<html lang="en" xmlns:f="http://typo3.org/ns/TYPO3/CMS/Fluid/ViewHelpers"',
+            'xmlns:jw="http://typo3.org/ns/JWeiland/VideoShariff/ViewHelpers"',
+            'data-namespace-typo3-fluid="true">',
+            '{jw:videoCreationDate(fileReference: \'{file}\')}',
+            '</html>'
+        ]));
+
+        $view->assign('file', $coreFileReference);
+        self::assertSame(
+            '1682928000',
+            trim($view->render())
+        );
+
+        $view->assign('file', $extbaseFileReference);
+        self::assertSame(
+            '1682928000',
+            trim($view->render())
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function videoCreateDateReturnCrdate(): void
+    {
+        $file = $this->createMock(File::class);
+        $file
+            ->expects(self::atLeastOnce())
+            ->method('getUid')
+            ->willReturn(1);
+        $file
+            ->expects(self::atLeastOnce())
+            ->method('getProperty')
+            ->willReturnMap([
+                ['content_creation_date', 0],
+                ['creation_date', 0],
+                ['crdate', 1683014400],
+            ]);
+
+        $coreFileReference = $this->createMock(FileReference::class);
+        $coreFileReference
+            ->expects(self::atLeastOnce())
+            ->method('getOriginalFile')
+            ->willReturn($file);
+
+        $extbaseFileReference = new ExtbaseFileReference();
+        $extbaseFileReference->setOriginalResource($coreFileReference);
+
+        $view = GeneralUtility::makeInstance(StandaloneView::class);
+        $view->setTemplateSource(implode(' ', [
+            '<html lang="en" xmlns:f="http://typo3.org/ns/TYPO3/CMS/Fluid/ViewHelpers"',
+            'xmlns:jw="http://typo3.org/ns/JWeiland/VideoShariff/ViewHelpers"',
+            'data-namespace-typo3-fluid="true">',
+            '{jw:videoCreationDate(fileReference: \'{file}\')}',
+            '</html>'
+        ]));
+
+        $view->assign('file', $coreFileReference);
+        self::assertSame(
+            '1683014400',
+            trim($view->render())
+        );
+
+        $view->assign('file', $extbaseFileReference);
+        self::assertSame(
+            '1683014400',
+            trim($view->render())
         );
     }
 }
