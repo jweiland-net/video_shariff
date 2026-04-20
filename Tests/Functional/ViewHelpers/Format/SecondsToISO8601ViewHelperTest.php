ee5b699bdde43a56dc7d78b1c684ee2298b478f7
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2026-04-20 13:17:11 +0200

    [TASK] Refactor and modernize unit tests for ViewHelpers in video_shariff extension

--- Tests/Functional/ViewHelpers/Format/SecondsToISO8601ViewHelperTest.php
+++ Tests/Functional/ViewHelpers/Format/SecondsToISO8601ViewHelperTest.php
@@ -11,43 +11,67 @@ declare(strict_types=1);
 
 namespace JWeiland\VideoShariff\Tests\Functional\ViewHelpers\Format;
 
+use JWeiland\VideoShariff\ViewHelpers\Format\SecondsToISO8601ViewHelper;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
 use TYPO3\CMS\Core\Http\ServerRequest;
 use TYPO3\CMS\Fluid\Core\Rendering\RenderingContextFactory;
 use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 use TYPO3Fluid\Fluid\View\TemplateView;
 
-/**
- * Test case.
- */
-class SecondsToISO8601ViewHelperTest extends FunctionalTestCase
+#[CoversClass(SecondsToISO8601ViewHelper::class)]
+final class SecondsToISO8601ViewHelperTest extends FunctionalTestCase
 {
     protected array $testExtensionsToLoad = [
         'typo3conf/ext/video_shariff',
     ];
 
+    /**
+     * @return array<string, array{int, string}>
+     */
     public static function secondsDataProvider(): array
     {
         return [
+            // Zero short-circuits to the ISO 8601 "zero duration" notation.
             '0 seconds will return P0S' => [0, 'P0S'],
+
+            // Sub-minute durations.
             '14 seconds will return PT14S' => [14, 'PT14S'],
             '59 seconds will return PT59S' => [59, 'PT59S'],
+
+            // Minute boundary and combinations.
             '60 seconds will return PT1M' => [60, 'PT1M'],
             '61 seconds will return PT1M1S' => [61, 'PT1M1S'],
             '250 seconds will return PT4M10S' => [250, 'PT4M10S'],
+
+            // Hour boundary.
             '3600 seconds will return PT1H' => [3600, 'PT1H'],
             '3700 seconds will return PT1H1M40S' => [3700, 'PT1H1M40S'],
+            '7200 seconds will return PT2H' => [7200, 'PT2H'],
             '43200 seconds will return PT12H' => [43200, 'PT12H'],
+
+            // Day boundary and multi-unit combinations.
             '86400 seconds will return P1D' => [86400, 'P1D'],
             '86600 seconds will return P1DT3M20S' => [86600, 'P1DT3M20S'],
+            '90060 seconds will return P1DT1H1M' => [90060, 'P1DT1H1M'],
+
+            // Multi-day duration.
+            '172800 seconds will return P2D' => [172800, 'P2D'],
         ];
     }
 
-    /**
-     * @test
-     *
-     * @dataProvider secondsDataProvider
-     */
+    #[Test]
+    #[DataProvider('secondsDataProvider')]
     public function formatSecondsToISO8601(int $seconds, string $expected): void
+    {
+        $view = $this->buildTemplateView();
+        $view->assign('seconds', $seconds);
+
+        self::assertSame($expected, trim($view->render()));
+    }
+
+    private function buildTemplateView(): TemplateView
     {
         $templateSource = implode(' ', [
             '<html lang="en" xmlns:f="http://typo3.org/ns/TYPO3/CMS/Fluid/ViewHelpers"',
@@ -60,12 +84,7 @@ class SecondsToISO8601ViewHelperTest extends FunctionalTestCase
         $request = new ServerRequest('http://localhost/', 'GET');
         $context = $this->get(RenderingContextFactory::class)->create([], $request);
         $context->getTemplatePaths()->setTemplateSource($templateSource);
-        $view = new TemplateView($context);
-        $view->assign('seconds', $seconds);
 
-        self::assertSame(
-            $expected,
-            trim($view->render()),
-        );
+        return new TemplateView($context);
     }
 }
--- Tests/Functional/ViewHelpers/VideoCreationDateViewHelperTest.php
+++ Tests/Functional/ViewHelpers/VideoCreationDateViewHelperTest.php
@@ -11,6 +11,11 @@ declare(strict_types=1);
 
 namespace JWeiland\VideoShariff\Tests\Functional\ViewHelpers;
 
+use JWeiland\VideoShariff\Traits\GetCoreFileReferenceTrait;
+use JWeiland\VideoShariff\ViewHelpers\VideoCreationDateViewHelper;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\UsesTrait;
 use TYPO3\CMS\Core\Http\ServerRequest;
 use TYPO3\CMS\Core\Resource\File;
 use TYPO3\CMS\Core\Resource\FileReference;
@@ -19,14 +24,105 @@ use TYPO3\CMS\Fluid\Core\Rendering\RenderingContextFactory;
 use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 use TYPO3Fluid\Fluid\View\TemplateView;
 
-/**
- * Test case.
- */
-class VideoCreationDateViewHelperTest extends FunctionalTestCase
+#[CoversClass(VideoCreationDateViewHelper::class)]
+#[UsesTrait(GetCoreFileReferenceTrait::class)]
+final class VideoCreationDateViewHelperTest extends FunctionalTestCase
 {
     protected array $testExtensionsToLoad = [
         'typo3conf/ext/video_shariff',
     ];
+
+    #[Test]
+    public function renderReturnsContentCreationDateWhenAvailable(): void
+    {
+        $this->assertRenderedValueForProperties(
+            [
+                'content_creation_date' => 1683100800,
+                'creation_date' => 0,
+                'crdate' => 0,
+            ],
+            '1683100800',
+        );
+    }
+
+    #[Test]
+    public function renderFallsBackToCreationDateWhenContentCreationDateIsEmpty(): void
+    {
+        $this->assertRenderedValueForProperties(
+            [
+                'content_creation_date' => 0,
+                'creation_date' => 1682928000,
+                'crdate' => 0,
+            ],
+            '1682928000',
+        );
+    }
+
+    #[Test]
+    public function renderFallsBackToCrdateWhenOtherDatesAreEmpty(): void
+    {
+        $this->assertRenderedValueForProperties(
+            [
+                'content_creation_date' => 0,
+                'creation_date' => 0,
+                'crdate' => 1683014400,
+            ],
+            '1683014400',
+        );
+    }
+
+    #[Test]
+    public function renderReturnsZeroWhenAllDatePropertiesAreEmpty(): void
+    {
+        $this->assertRenderedValueForProperties(
+            [
+                'content_creation_date' => 0,
+                'creation_date' => 0,
+                'crdate' => 0,
+            ],
+            '0',
+        );
+    }
+
+    #[Test]
+    public function renderReturnsZeroWhenFileReferenceIsNotAFileReferenceObject(): void
+    {
+        $view = $this->buildTemplateView($this->getTemplateSource());
+        $view->assign('file', new \stdClass());
+
+        self::assertSame('0', trim($view->render()));
+    }
+
+    /**
+     * Renders the template against both a core and an Extbase FileReference built from
+     * the given property map, asserting that the rendered timestamp matches $expected
+     * in both cases.
+     *
+     * @param array<string, int> $properties
+     */
+    private function assertRenderedValueForProperties(array $properties, string $expected): void
+    {
+        $propertyMap = [];
+        foreach ($properties as $key => $value) {
+            $propertyMap[] = [$key, $value];
+        }
+
+        $file = $this->createMock(File::class);
+        $file->method('getUid')->willReturn(1);
+        $file->method('getProperty')->willReturnMap($propertyMap);
+
+        $coreFileReference = $this->createMock(FileReference::class);
+        $coreFileReference->method('getOriginalFile')->willReturn($file);
+
+        $extbaseFileReference = new ExtbaseFileReference();
+        $extbaseFileReference->setOriginalResource($coreFileReference);
+
+        foreach ([$coreFileReference, $extbaseFileReference] as $fileReference) {
+            $view = $this->buildTemplateView($this->getTemplateSource());
+            $view->assign('file', $fileReference);
+            self::assertSame($expected, trim($view->render()));
+        }
+    }
 
     private function buildTemplateView(string $templateSource): TemplateView
     {
@@ -46,116 +142,5 @@ class VideoCreationDateViewHelperTest extends FunctionalTestCase
             '{jw:videoCreationDate(fileReference: \'{file}\')}',
             '</html>',
         ]);
-    }
-
-    /**
-     * @test
-     */
-    public function videoCreateDateReturnContentCreationDate(): void
-    {
-        $file = $this->createMock(File::class);
-        $file
-            ->expects($this->atLeastOnce())
-            ->method('getUid')
-            ->willReturn(1);
-        $file
-            ->expects($this->atLeastOnce())
-            ->method('getProperty')
-            ->willReturnMap([
-                ['content_creation_date', 1683100800],
-                ['creation_date', 0],
-                ['crdate', 0],
-            ]);
-
-        $coreFileReference = $this->createMock(FileReference::class);
-        $coreFileReference
-            ->expects($this->atLeastOnce())
-            ->method('getOriginalFile')
-            ->willReturn($file);
-
-        $extbaseFileReference = new ExtbaseFileReference();
-        $extbaseFileReference->setOriginalResource($coreFileReference);
-
-        $view = $this->buildTemplateView($this->getTemplateSource());
-        $view->assign('file', $coreFileReference);
-        self::assertSame('1683100800', trim($view->render()));
-
-        $view = $this->buildTemplateView($this->getTemplateSource());
-        $view->assign('file', $extbaseFileReference);
-        self::assertSame('1683100800', trim($view->render()));
-    }
-
-    /**
-     * @test
-     */
-    public function videoCreateDateReturnCreationDate(): void
-    {
-        $file = $this->createMock(File::class);
-        $file
-            ->expects($this->atLeastOnce())
-            ->method('getUid')
-            ->willReturn(1);
-        $file
-            ->expects($this->atLeastOnce())
-            ->method('getProperty')
-            ->willReturnMap([
-                ['content_creation_date', 0],
-                ['creation_date', 1682928000],
-                ['crdate', 0],
-            ]);
-
-        $coreFileReference = $this->createMock(FileReference::class);
-        $coreFileReference
-            ->expects($this->atLeastOnce())
-            ->method('getOriginalFile')
-            ->willReturn($file);
-
-        $extbaseFileReference = new ExtbaseFileReference();
-        $extbaseFileReference->setOriginalResource($coreFileReference);
-
-        $view = $this->buildTemplateView($this->getTemplateSource());
-        $view->assign('file', $coreFileReference);
-        self::assertSame('1682928000', trim($view->render()));
-
-        $view = $this->buildTemplateView($this->getTemplateSource());
-        $view->assign('file', $extbaseFileReference);
-        self::assertSame('1682928000', trim($view->render()));
-    }
-
-    /**
-     * @test
-     */
-    public function videoCreateDateReturnCrdate(): void
-    {
-        $file = $this->createMock(File::class);
-        $file
-            ->expects($this->atLeastOnce())
-            ->method('getUid')
-            ->willReturn(1);
-        $file
-            ->expects($this->atLeastOnce())
-            ->method('getProperty')
-            ->willReturnMap([
-                ['content_creation_date', 0],
-                ['creation_date', 0],
-                ['crdate', 1683014400],
-            ]);
-
-        $coreFileReference = $this->createMock(FileReference::class);
-        $coreFileReference
-            ->expects($this->atLeastOnce())
-            ->method('getOriginalFile')
-            ->willReturn($file);
-
-        $extbaseFileReference = new ExtbaseFileReference();
-        $extbaseFileReference->setOriginalResource($coreFileReference);
-
-        $view = $this->buildTemplateView($this->getTemplateSource());
-        $view->assign('file', $coreFileReference);
-        self::assertSame('1683014400', trim($view->render()));
-
-        $view = $this->buildTemplateView($this->getTemplateSource());
-        $view->assign('file', $extbaseFileReference);
-        self::assertSame('1683014400', trim($view->render()));
     }
 }
