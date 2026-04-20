246929eb51f2b1455d7adcc9238f7ed522670685
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2026-04-20 12:22:32 +0200

    [TASK] Reintroduce 'var' directory exclusion in php-cs-fixer configuration

--- Build/cgl/config.php
+++ Build/cgl/config.php
@@ -168,6 +168,7 @@ return (new \PhpCsFixer\Config())
     ->setParallelConfig(\PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
     ->setFinder(
         (new PhpCsFixer\Finder())
+            ->exclude(['var',])
             ->ignoreVCSIgnored(true)
             ->in(__DIR__ . '/../../'),
     )
